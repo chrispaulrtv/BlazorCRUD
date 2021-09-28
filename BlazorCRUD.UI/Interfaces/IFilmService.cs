7845ac546c8c476f981dc2ceaf8c2440fcd00faa
ChrisPaulRTV <creinoso7982@gmail.com>
2021-09-28 12:11:15 -0500

    Add project files.

---
+++ BlazorCRUD.UI/Interfaces/IFilmService.cs
@@ -0,0 +1,21 @@
+﻿using BlazorCRUD.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.UI.Interfaces
+{
+    public interface IFilmService
+    {
+        Task<IEnumerable<Film>> GetAllFilms();
+
+        Task<Film> GetDetails(int Id);
+
+        Task<bool> DeleteFilm(int Id);
+
+        Task<bool> SaveFilm(Film film);
+
+
+    }
+}
